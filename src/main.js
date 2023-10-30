2621b1af26061943fe7ad8e3b4465b4968fd4448
Anton Kotorovych <akotorovych@gmail.com>
2023-10-30 02:33:00 +0200

    implement numbers appear when numbers clicked

--- src/main.js
+++ src/main.js
@@ -1,14 +1,40 @@
 'use strict';
+const displayMain = document.getElementById('displayMain');
+const displaySecondary = document.getElementById('displaySecondary');
 
 const BUTTONS = document.querySelectorAll('.keyboard li button');
-console.log(BUTTONS);
+
+const NUMBERS = document.querySelectorAll('[data-numbers]');
+
+let displayValue = 0;
+let result = '';
+
+displayMain.innerHTML = displayValue;
+displaySecondary.innerHTML = result;
+
 document.addEventListener('keydown', event => {
   BUTTONS.forEach(button => {
-    if (button.value == event.key) {
-      button.classList.add('button--clicked');
-      setTimeout(() => {
-        button.classList.remove('button--clicked');
-      }, 70);
+    if (button.value == event.key) button.classList.add('button--clicked');
+  });
+});
+
+document.addEventListener('keyup', event => {
+  if (displayValue === 0) displayValue = '';
+  BUTTONS.forEach(button => {
+    if (button.dataset.numbers === event.key) {
+      button.classList.remove('button--clicked');
+      displayValue += button.dataset.numbers;
+      displayMain.innerHTML = displayValue;
     }
   });
 });
+
+// document.addEventListener('mouseup', event => {
+//   BUTTONS.forEach(button => {
+//     if (button.value == event.target.value) {
+//       button.classList.remove('button--clicked');
+//       displayValue = displayValue + +event.target.value;
+//       console.log(displayValue);
+//     }
+//   });
+// });
