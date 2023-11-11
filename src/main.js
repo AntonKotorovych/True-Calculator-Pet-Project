884012f349bc2a0d453c59a43e2702369e40279a
Anton Kotorovych <akotorovych@gmail.com>
2023-11-11 02:54:20 +0200

    remove buttons all from logic

--- src/main.js
+++ src/main.js
@@ -103,138 +103,139 @@ document.addEventListener('keyup', event => {
   }
 
   if (displayValue.length < 2) isDot = false;
+  // NUMBERS
 
-  BUTTONS.forEach(button => {
-    if (event.key === '0' && displayValue === '0') {
-      return;
-    }
-    if (event.key === '.' && operators.includes(displayValue[displayValue.length - 1])) {
-      return;
-    }
-    if (event.key === '.' && isDot) {
-      return;
-    }
+  if (event.key === '0' && displayValue === '0') {
+    return;
+  }
+  if (event.key === '.' && operators.includes(displayValue[displayValue.length - 1])) {
+    return;
+  }
+  if (event.key === '.' && isDot) {
+    return;
+  }
 
-    if (event.key === NUMBERS.button.value) {
-      console.log('number');
-      numbersHandler(button);
+  if (event.key === NUMBERS.button.value) {
+    console.log('number');
+    numbersHandler(button);
+    return;
+  }
+
+  // OPERATORS
+  if (button.dataset.operators === event.key) {
+    if (isResultEqual) hasFirstOperatorAfterEqual = true;
+    if (isResultEqual && hasFirstOperatorAfterEqual) isResultEqual = false;
+    if (operators.includes(displayValue[displayValue.length - 1])) {
       return;
     }
+    isDot = false;
 
-    if (button.dataset.operators === event.key) {
-      if (isResultEqual) hasFirstOperatorAfterEqual = true;
-      if (isResultEqual && hasFirstOperatorAfterEqual) isResultEqual = false;
-      if (operators.includes(displayValue[displayValue.length - 1])) {
-        return;
-      }
+    displayValue += button.value;
+    displayMain.textContent = displayValue;
+  }
+
+  if (button.dataset.deleting === event.key) {
+    if (button.dataset.deleting === 'Backspace') {
+      if (displayValue[displayValue.length - 1] === '.') isDot = false;
+
+      displayValue = displayValue.slice(0, -1);
+      if (displayValue === '') displayValue = '0';
+      displayMain.textContent = displayValue;
+    } else {
       isDot = false;
+      isResultEqual = false;
+      hasFirstOperandAfterEqual = false;
+      hasFirstOperatorAfterEqual = false;
 
-      displayValue += button.value;
+      displayValue = '0';
       displayMain.textContent = displayValue;
     }
+  }
+  if (button.dataset.equal === event.key) {
+    if (operators.includes(displayValue[displayValue.length - 1])) return;
 
-    if (button.dataset.deleting === event.key) {
-      if (button.dataset.deleting === 'Backspace') {
-        if (displayValue[displayValue.length - 1] === '.') isDot = false;
+    // Working with results
 
-        displayValue = displayValue.slice(0, -1);
-        if (displayValue === '') displayValue = '0';
-        displayMain.textContent = displayValue;
-      } else {
-        isDot = false;
-        isResultEqual = false;
-        hasFirstOperandAfterEqual = false;
-        hasFirstOperatorAfterEqual = false;
+    function calculateExpression(expression) {
+      // Replacing '×' and '÷' to * and /
 
-        displayValue = '0';
-        displayMain.textContent = displayValue;
-      }
-    }
-    if (button.dataset.equal === event.key) {
-      if (operators.includes(displayValue[displayValue.length - 1])) return;
+      const expressionString = expression.replace(/×/g, '*').replace(/÷/g, '/');
 
-      // Working with results
+      // Parse string in the comfortable array format to work with
 
-      function calculateExpression(expression) {
-        // Replacing '×' and '÷' to * and /
+      function prepareMathExpression(expression) {
+        let counter = 0;
+        let separatedString = '';
+        const operators = ['+', '-', '*', '/'];
 
-        const expressionString = expression.replace(/×/g, '*').replace(/÷/g, '/');
-
-        // Parse string in the comfortable array format to work with
-
-        function prepareMathExpression(expression) {
-          let counter = 0;
-          let separatedString = '';
-          const operators = ['+', '-', '*', '/'];
-
-          if (expression[0].startsWith('-')) {
-            counter = 1;
-            // Parsing anyway the first operand if it is negative (starts with the '-' operator)
-            separatedString += expression[0];
-          }
-          for (counter; counter < expression.length; counter++) {
-            !operators.includes(expression[counter]) ? (separatedString += expression[counter]) : (separatedString += ` ${expression[counter]} `);
-          }
-          return separatedString.split(' ');
+        if (expression[0].startsWith('-')) {
+          counter = 1;
+          // Parsing anyway the first operand if it is negative (starts with the '-' operator)
+          separatedString += expression[0];
         }
+        for (counter; counter < expression.length; counter++) {
+          !operators.includes(expression[counter]) ? (separatedString += expression[counter]) : (separatedString += ` ${expression[counter]} `);
+        }
+        return separatedString.split(' ');
+      }
 
-        const mathExpressionArray = prepareMathExpression(expressionString);
-        // mutating expression array with calculating results * and / operands pairs
+      const mathExpressionArray = prepareMathExpression(expressionString);
+      // mutating expression array with calculating results * and / operands pairs
 
-        function multiplyOrDivideOperands(expression, operator) {
-          while (expression.includes(`${operator}`)) {
-            for (let i = 1; i < expression.length; i += 2) {
-              const currentOperator = expression[i];
-              if (currentOperator === `${operator}`) {
-                let result = 0;
-                if (operator === '*') {
-                  result = expression[i - 1] * expression[i + 1];
-                } else if (operator === '/') {
-                  result = expression[i - 1] / expression[i + 1];
-                }
-                expression.splice(i - 1, 3, result);
+      function multiplyOrDivideOperands(expression, operator) {
+        while (expression.includes(`${operator}`)) {
+          for (let i = 1; i < expression.length; i += 2) {
+            const currentOperator = expression[i];
+            if (currentOperator === `${operator}`) {
+              let result = 0;
+              if (operator === '*') {
+                result = expression[i - 1] * expression[i + 1];
+              } else if (operator === '/') {
+                result = expression[i - 1] / expression[i + 1];
               }
+              expression.splice(i - 1, 3, result);
             }
           }
         }
-
-        multiplyOrDivideOperands(mathExpressionArray, '*');
-        multiplyOrDivideOperands(mathExpressionArray, '/');
-
-        // calculating result
-
-        let result = parseFloat(mathExpressionArray[0]);
-
-        for (let i = 1; i < mathExpressionArray.length; i += 2) {
-          const operator = mathExpressionArray[i];
-          const operand = parseFloat(mathExpressionArray[i + 1]);
-          operator === '+' ? (result += operand) : (result -= operand);
-        }
-        return result;
       }
 
-      const expressionResult = calculateExpression(displayValue);
-      console.log(displayValue);
-      console.log(expressionResult);
+      multiplyOrDivideOperands(mathExpressionArray, '*');
+      multiplyOrDivideOperands(mathExpressionArray, '/');
 
-      if (expressionResult === Infinity || expressionResult === -Infinity) {
-        displayMain.textContent = 'numbers are not divisible by zero';
-        displayValue = '0';
-        return;
+      // calculating result
+
+      let result = parseFloat(mathExpressionArray[0]);
+
+      for (let i = 1; i < mathExpressionArray.length; i += 2) {
+        const operator = mathExpressionArray[i];
+        const operand = parseFloat(mathExpressionArray[i + 1]);
+        operator === '+' ? (result += operand) : (result -= operand);
       }
-
-      displayMain.textContent = expressionResult;
-      displayValue = displayMain.textContent;
-
-      if (displayValue.includes('.')) {
-        isDot = true;
-      } else {
-        isDot = false;
-      }
-
-      isResultEqual = true;
+      return result;
     }
-  });
+
+    const expressionResult = calculateExpression(displayValue);
+    console.log(displayValue);
+    console.log(expressionResult);
+
+    if (expressionResult === Infinity || expressionResult === -Infinity) {
+      displayMain.textContent = 'numbers are not divisible by zero';
+      displayValue = '0';
+      return;
+    }
+
+    displayMain.textContent = expressionResult;
+    displayValue = displayMain.textContent;
+
+    if (displayValue.includes('.')) {
+      isDot = true;
+    } else {
+      isDot = false;
+    }
+
+    isResultEqual = true;
+  }
+
   if (displayValue.length >= 24) {
     BUTTONS.forEach(button => button.classList.remove('button--clicked'));
     displayValue = displayValue.slice(0, -1);
